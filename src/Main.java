712515156078989ae697ad5c6f97de8c770654ea
gvp1965y <gvp1965y@yandex.ru>
2025-09-23 20:40:48 +0300

    ver. 5.0.0.1

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,41 @@
-//#SAMPLE@BOBA: int task01Id = tm.insTask(task01); etc. subTask, epic
-//#SAMPLE@BOBA: boolean updated = tm.updTask(task01)); etc. subTask, epic
+//#DEMO@BOBA: task, taskId; subTask, subTaskId; epic, epicId: изменяемые элементы
+//#DEMO@BOBA: taskUpdated; subTaskUpdated; epicUpdated : успешное обновление элемента
 
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Поехали!");
 
         //TaskManager tm = new InMemoryTaskManager();
         TaskManager tm = Managers.getDefault();
         printAll(tm, "После создания --------------");
 
-        Task task01 = new Task("task01", null, TaskStatus.NEW);
-        tm.insTask(task01);
-        Task task02 = new Task("task02", null, TaskStatus.IN_PROGRESS);
-        tm.insTask(task02);
-        Epic epic04 = new Epic("epic04", null, TaskStatus.NEW);
-        tm.insEpic(epic04);
-        SubTask subTask0401 = new SubTask("subTask0401", null, TaskStatus.NEW, epic04.getId());
-        tm.insSubTask(subTask0401);
-        SubTask subTask0402 = new SubTask("subTask0402", null, TaskStatus.IN_PROGRESS, epic04.getId());
-        tm.insSubTask(subTask0402);
-        Epic epic05 = new Epic("epic05", null, TaskStatus.NEW);
-        tm.insEpic(epic05);
-        SubTask subTask0501 = new SubTask("subTask0501", null, TaskStatus.DONE, epic05.getId());
-        tm.insSubTask(subTask0501);
+        Task task = new Task("task01", null, TaskStatus.NEW);
+        int taskId = tm.insTask(task);
+        tm.insTask(new Task("task02", null, TaskStatus.IN_PROGRESS));
+        Epic epic = new Epic("epic04", null, TaskStatus.NEW);
+        int epicId = tm.insEpic(epic);
+        SubTask subTask = new SubTask("subTask0401", null, TaskStatus.NEW, epicId);
+        int subTaskId = tm.insSubTask(subTask);
+        tm.insSubTask(new SubTask("subTask0402", null, TaskStatus.DONE, epicId));
+        tm.insSubTask(new SubTask("subTask0501", null, TaskStatus.DONE, tm.insEpic(new Epic("epic05", null, TaskStatus.NEW))));
         printAll(tm, "После вставки --------------");
 
-        task01.setStatus(TaskStatus.IN_PROGRESS);
-        tm.updTask(task01);
-        subTask0401.setStatus(TaskStatus.IN_PROGRESS);
-        tm.updSubTask(subTask0401);
-        epic04.setStatus(TaskStatus.NEW);
-        tm.updEpic(epic04);
+        task.setStatus(TaskStatus.IN_PROGRESS);
+        boolean taskUpdated = tm.updTask(task);
+        if (!taskUpdated) { printNotUpdated(task); }
+        subTask.setStatus(TaskStatus.IN_PROGRESS);
+        boolean subTaskUpdated = tm.updSubTask(subTask);
+        if (!subTaskUpdated) { printNotUpdated(subTask); }
+        epic.setStatus(TaskStatus.NEW);
+        boolean epicUpdated = tm.updEpic(epic);
+        if (!epicUpdated) { printNotUpdated(epic); }
         printAll(tm, "После изменения Статусов ------------");
 
-        tm.delTask(task01.getId());
-        tm.delSubTask(subTask0401.getId());
-        tm.delEpic(epic04.getId());
+        tm.delTask(taskId);
+        tm.delSubTask(subTaskId);
+        tm.delEpic(epicId);
         printAll(tm, "После удаления ------------");
 
         tm.delTasks();
@@ -71,5 +70,9 @@ public class Main {
             }
         }
         System.out.println("  History " + tm.getHistory()); //#DEBUG@BOBA
+    }
+
+    static void printNotUpdated(Task task) {
+        System.out.println("Ошибка обновления " + task);
     }
 }
