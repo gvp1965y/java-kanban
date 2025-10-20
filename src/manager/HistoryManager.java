5de36e63742261a19d7e04e90ff6ba570a615f7a
gvp1965y <gvp1965y@yandex.ru>
2025-10-20 20:27:21 +0300

    ver. 6.0.0.1

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -2,10 +2,15 @@ package manager;
 
 import tasks.*;
 import java.util.List;
+import java.util.Set;
 
 public interface HistoryManager {
 
     void add(Task task);
+
+    void remove(int id);
+
+    void removeNodes(Set<Integer> ids);
 
     List<Task> getHistory();
 }
--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,27 +1,101 @@
 package manager;
 
-import tasks.*;
-import java.util.LinkedList;
+import tasks.Task;
+
+import java.util.Set;
+import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class InMemoryHistoryManager implements HistoryManager{
-    int TASK_HISTORY_MAX = 10;            //#ASK@BOBA: public static final
-    private final List<Task> taskHistory; //#ASK@BOBA: final
+    private Node head;
+    private Node tail;
+    private final Map<Integer, Node> nodeMap;
 
     public InMemoryHistoryManager() {
-        taskHistory = new LinkedList<>(); //#ASK@BOBA: LinkedList vs ArrayList
+        nodeMap = new HashMap<>();
     }
 
     @Override
     public void add(Task task) {
         if (task == null) { return; }
-
-        if (taskHistory.size() == TASK_HISTORY_MAX) {
-            taskHistory.removeFirst();
-        }
-        taskHistory.add(new Task(task));    //#ASK@BOBA:  history add clone task
+        removeNode(task.getId());
+        setTail(task);
     }
 
     @Override
-    public List<Task> getHistory() { return taskHistory; }
+    public void remove(int id) {
+        removeNode(id);
+    }
+
+    @Override
+    public void removeNodes(Set<Integer> ids) {
+        Iterator<Integer> i = ids.iterator();
+        while (i.hasNext()) {
+            Integer id = i.next();
+            i.remove();
+            removeNode(id);
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return getTasks();
+    }
+
+    private void setTail(Task task) {
+        if (nodeMap.isEmpty()) {
+            Node node = new Node(null, task, null); //#ASK@BOBA: clone task
+            nodeMap.put(task.getId(), node);
+            head = node;
+            tail = node;
+        } else {
+            Node node = new Node(tail, task, null); //#ASK@BOBA: clone task
+            nodeMap.put(task.getId(), node);
+            node.prev.next = node;
+            tail = node;
+        }
+    }
+
+    private List<Task> getTasks() {
+        List<Task> taskList = new ArrayList<>(nodeMap.size());
+        Node node = head;
+        int idx = 0;
+        while (node != null) {
+            Task task = node.data;
+            taskList.add(idx, task);
+            node = node.next;
+            idx++;
+        }
+        return taskList;
+    }
+
+    private void removeNode(int id) {
+        final Node node = nodeMap.remove(id);
+        if (node == null) {
+            return;
+        }
+        if (head == tail) {
+            head = null;
+            tail = null;
+        }
+        else if (node == head) {
+            head = node.next;
+            if (node.next != null) {
+                node.next.prev = null;
+            }
+        }
+        else if (node == tail) {
+            tail = node.prev;
+            if (node.prev != null) {
+                node.prev.next = null;
+            }
+        }
+        else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+    }
 }
