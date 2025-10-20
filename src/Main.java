08ba4b51114876ccc2e6577b872cf851cc3a9db8
gvp1965y <gvp1965y@yandex.ru>
2025-10-20 20:48:38 +0300

    ver. 6.0.0.4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//#DEMO@BOBA: task, taskId; subTask, subTaskId; epic, epicId: изменяемые элементы
+//#DEMO@BOBA: task, taskId; subTask, subTaskId; epic, epicId: изменяемые элементы (ерунда какая то)
 //#DEMO@BOBA: taskUpdated; subTaskUpdated; epicUpdated : успешное обновление элемента
 
 import tasks.*;
